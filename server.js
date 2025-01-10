d74f6eaf25077f7208d67f6d98946dbe63af7627
akhilkongonda <akhilkongonda84@gmail.com>
2025-01-11 00:44:02 +0530

    solving db issue while deploying

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { fetchCryptoData } = require('./backgroundJob');
 const app = express();
 
+
+
 // Import and connect to MongoDB first
-require('./config/db');  // This could be your MongoDB connection file
+require('./db');  // This could be your MongoDB connection file
+
+
+
+// Schedule the background job to run every 2 hours
+fetchCryptoData();
 
 // After database connection is set up, define routes and controllers
-const cryptoRoutes = require('./routers/cryptoRoutes');
+const cryptoRoutes = require('./routers/cryptorouter')
 app.use('/api', cryptoRoutes);
+
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
