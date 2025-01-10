05985f06d52066eb4cc753522ac99f37412f0426
akhilkongonda <akhilkongonda84@gmail.com>
2025-01-11 00:49:06 +0530

    solving db issue while deploying1

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
 })
 .then(() => console.log('MongoDB connected!'))
 .catch((err) => console.error('Error connecting to MongoDB:', err));
